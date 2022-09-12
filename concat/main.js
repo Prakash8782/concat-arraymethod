8170f8a8ffad23759804810a81f69e35741490ca
Prakash8782 <prakash8782@gmail.com>
2022-09-12 12:30:37 +0530

    concat

---
+++ concat/main.js
@@ -0,0 +1,8 @@
+const color1=["Red","Blue","Green","Yellow","Pink"];
+const color2=["Black","White"];
+const colors=color1.concat(color2);
+console.log("colors : " ,color1);
+console.log("length of an array : "+color1.length);
+console.log("method : array.concat(color2)");
+console.log("change in array : ",colors);
+console.log("return : length of array after concat(); is "+colors.length);
